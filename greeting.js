41426ce3fd68f53c9739dbf58e5ff4c079acd95d
VivekThazhathattil <tvivek@iisc.ac.in>
2022-07-08 14:01:42 +0530

    fix proper greeting

---
+++ greeting.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function() {
+    $("#login").text(function(){
+        var now = new Date();
+        hours = now.getHours();
+        var timeOfDay = ""
+        if(hours >= 16)
+            timeOfDay = "Evening";
+        else if(hours < 12)
+            timeOfDay = "Morning";
+        else
+            timeOfDay = "Afternoon";
+        return "Hello!" + " Good " + timeOfDay + "!";
+    });
+});
